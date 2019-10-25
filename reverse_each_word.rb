0bd007d3110453d8de6c3bcacfec3d001f02037f
Hans Foster <fosterhans@gmail.com>
2019-10-25 18:49:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(word)
   word_array = word.split(" ")
   new_word = ""
   new_word_array = []
-  word_array.each do |each_word|
+  word_array.collect do |each_word|
     new_word_array.push(each_word.reverse)
   end
   new_word = new_word_array.join(" ")
