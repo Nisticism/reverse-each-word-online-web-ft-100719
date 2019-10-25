96ccf325e6d945c5b0a614dfcdc3fd176bfc8c1d
Hans Foster <fosterhans@gmail.com>
2019-10-25 18:42:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
 def reverse_each_word(word)
   word_array = word.split(" ")
-  word_array.reverse
-  new_word =
+  new_word = ""
+  word_array.each do |each_word|
+    new_word += each_word.reverse 
+  end
+  new_word
+end
+
